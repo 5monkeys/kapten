e11a5268b7bf246c41bf91eab0f1c2d3ec0e7a34
Jonas Lundberg <jonas@5monkeys.se>
2019-10-21 16:43:00 +0200

    Change argument order in help

--- README.md
+++ README.md
@@ -9,26 +9,27 @@ Updates a Docker Swarm service when a new image is available.
 **Usage:**
 ```sh
 $ kapten --help
-usage: kapten [-h] -s SERVICES [-p PROJECT] [--slack-token SLACK_TOKEN]
-              [--slack-channel SLACK_CHANNEL] [--check] [--force]
-              [-v VERBOSITY]
+usage: kapten [-h] [--version] [-s SERVICES] [-p PROJECT]
+              [--slack-token SLACK_TOKEN] [--slack-channel SLACK_CHANNEL]
+              [--check] [--force] [-v VERBOSITY]
 
 Checks for new images and updates services if needed.
 
 optional arguments:
   -h, --help            show this help message and exit
+  --version             Show version and exit.
   -s SERVICES, --service SERVICES
-                        Service to update
+                        Service to update.
   -p PROJECT, --project PROJECT
-                        Optional project name
+                        Optional project name.
   --slack-token SLACK_TOKEN
-                        Slack token to use for notification
+                        Slack token to use for notification.
   --slack-channel SLACK_CHANNEL
-                        Optional Slack channel to use for notification
-  --check               Only check if service needs to be updated
-  --force               Force service update
+                        Optional Slack channel to use for notification.
+  --check               Only check if service needs to be updated.
+  --force               Force service update.
   -v VERBOSITY, --verbosity VERBOSITY
-                        Level of verbosity
+                        Level of verbosity.
 ```
 
 **Example:**
--- kapten/cli.py
+++ kapten/cli.py
@@ -15,37 +15,42 @@ def command(input_args=None):
         description="Checks for new images and updates services if needed."
     )
     parser.add_argument(
+        "--version",
+        dest="show_version",
+        action="store_true",
+        help="Show version and exit.",
+    )
+    parser.add_argument(
         "-s",
         "--service",
         type=str,
         action="append",
         dest="services",
-        help="Service to update",
+        help="Service to update.",
     )
-    parser.add_argument("-p", "--project", type=str, help="Optional project name")
+    parser.add_argument("-p", "--project", type=str, help="Optional project name.")
     parser.add_argument(
-        "--slack-token", type=str, help="Slack token to use for notification"
+        "--slack-token", type=str, help="Slack token to use for notification."
     )
     parser.add_argument(
         "--slack-channel",
         type=str,
-        help="Optional Slack channel to use for notification",
+        help="Optional Slack channel to use for notification.",
     )
     parser.add_argument(
-        "--check", action="store_true", help="Only check if service needs to be updated"
+        "--check",
+        action="store_true",
+        help="Only check if service needs to be updated.",
     )
-    parser.add_argument("--force", action="store_true", help="Force service update")
+    parser.add_argument("--force", action="store_true", help="Force service update.")
     parser.add_argument(
-        "-v", "--verbosity", type=int, help="Level of verbosity", default=1
-    )
-    parser.add_argument(
-        "-V", "--version", action="store_true", help="Show version and exit"
+        "-v", "--verbosity", type=int, default=1, help="Level of verbosity."
     )
 
     args = parser.parse_args(input_args)
 
     # Show version
-    if args.version:
+    if args.show_version:
         print("Kapten {}".format(__version__))
         exit(0)
 
