04c75d137e0734c0c8e709cab8908486eeda48a4
Jonas Lundberg <jonas@5monkeys.se>
2019-12-13 10:09:06 +0100

    Replace make with nox

--- Makefile
+++ Makefile
@@ -1,45 +1,15 @@
 .PHONY: test
 test:
-	python setup.py test $(test)
-
-
-.PHONY: coverage
-coverage:
-	coverage run setup.py test
-	coverage report
-	coverage xml
-
-
-.PHONY: lint
-lint:
-	flake8 kapten --exit-zero
-
-
-.PHONY: format
-format:
-	black kapten tests
-	autoflake -r -i --remove-all-unused-imports kapten tests
-	isort -rc kapten tests
-
+	nox
 
 .PHONY: clean
 clean:
-	rm -rf dist
-	rm -rf *.egg-info
+	rm -rf dist kapten.egg-info
 
-
-.PHONY: publish
-publish: clean
+.PHONY: build
+build: clean
 	python setup.py sdist bdist_wheel
+
+.PHONY: release
+release: build
 	python -m twine upload dist/*
-
-
-.PHONY: requirements
-requirements:
-	pip-compile \
-		--upgrade --pre --generate-hashes \
-		--output-file requirements.dev.txt \
-		requirements.dev.in
-	chown \
-		--reference requirements.dev.in \
-		requirements.dev.txt
---
+++ noxfile.py
@@ -0,0 +1,46 @@
+import nox
+
+nox.options.stop_on_first_error = True
+nox.options.reuse_existing_virtualenvs = True
+nox.options.keywords = "test + check"
+
+source_files = ("kapten", "tests", "setup.py", "noxfile.py")
+lint_requirements = ("flake8", "black", "isort")
+
+
+@nox.session(python=["3.6", "3.7", "3.8"])
+def test(session):
+    session.install(
+        "--upgrade",
+        "pytest",
+        "pytest-asyncio",
+        "pytest-cov",
+        "asynctest",
+        "respx>=0.8.1",
+        "responses",
+    )
+    session.install("-e", ".[server]")
+
+    session.run("pytest", "-v", *session.posargs)
+
+
+@nox.session
+def check(session):
+    session.install("--upgrade", "flake8-bugbear", "mypy", *lint_requirements)
+    session.install("-e", ".[server]")
+
+    session.run("black", "--check", "--diff", "--target-version=py36", *source_files)
+    session.run("isort", "--check", "--diff", "--project=kapten", "-rc", *source_files)
+    session.run("flake8", *source_files)
+    session.run("mypy", "kapten")
+
+
+@nox.session
+def lint(session):
+    session.install("--upgrade", "autoflake", *lint_requirements)
+
+    session.run("autoflake", "--in-place", "--recursive", *source_files)
+    session.run("isort", "--project=kapten", "--recursive", "--apply", *source_files)
+    session.run("black", "--target-version=py36", *source_files)
+
+    check(session)
